651d7782d1a6972d3c402e320ae256e295d12a53
vedinYP <ypvedin@gmail.com>
2026-09-05 22:54:30 +0530

    Add files via upload

---
+++ 08_const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() { 
+     const double PI = 3.14159;
+     const int Light_Speed = 299792458;
+     const int WIDTH = 1920;
+     const int HEIGHT = 1080;
+
+     std::cout << "HEIGHT: " << HEIGHT << '\n';
+     std::cout << "WIDTH: " << WIDTH << '\n';
+     std::cout << "LIGHT_SPEED: " << Light_Speed << '\n';
+     std::cout << "PI: " << PI << '\n';
+     
+     return 0;
+}
