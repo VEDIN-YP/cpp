4e5be206bb2ba61a121cce94ec6dbc08723c56fe
vedinYP <ypvedin@gmail.com>
2026-09-05 22:53:40 +0530

    Add files via upload

---
+++ 05_helloworldex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment 
+    /*
+this is a 
+multi-
+line 
+comment        
+    */
+    std::cout << "hello world" << std::endl;
+    std::cout << "it's a beautiful rainy day" << std::endl;
+    return 0;
+}
