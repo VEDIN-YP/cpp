323c09dd5e775875ae6c6597261606122714a3de
vedinYP <ypvedin@gmail.com>
2026-09-05 22:54:16 +0530

    Add files via upload

---
+++ 07_variables.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+
+    // integer variable declaration
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    //double (number including decimal) 
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    // boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string address = "6969 fake st.";
+
+    std::cout << "Hello lil " << name <<'\n';
+    std::cout << "You are " << age  << "years old"; 
+
+    return 0;
+}   
